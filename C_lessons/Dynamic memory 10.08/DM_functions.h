e9dcac697d273ca423600f5b835957252a4923c6
alexl-87 <34104341+alexl-87@users.noreply.github.com>
2019-08-11 17:24:32 +0300

    Add files via upload

---
+++ C_lessons/Dynamic memory 10.08/DM_functions.h
@@ -0,0 +1,17 @@
+#ifndef DM_functions
+#define DM_functions
+
+typedef struct d_array{
+	int* arr;
+	int capacity;
+	int index;
+}d_array;
+
+
+
+int insert(d_array* array, int* index, int number);
+void print_array(d_array* array);
+d_array* array_builder (int capacity);
+void destroy(d_array* array);
+
+#endif
