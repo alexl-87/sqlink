6154f249eeaa18bee35112206e678a21dd9ef6a1
alexl-87 <34104341+alexl-87@users.noreply.github.com>
2019-08-14 07:29:54 +0300

    Add files via upload

---
+++ C_lessons/Files 13.08/File_functions.h
@@ -0,0 +1,29 @@
+#ifndef File_functions
+#define File_functions
+
+typedef struct Word{
+	
+	char* str;
+	int counter;
+
+}Word;
+
+typedef struct WordList{
+	
+	Word** word_list;
+	int capacity;
+	int num_of_words;
+
+}WordList;
+
+
+WordList* createList(int capacity);
+Word* create_word(char* str);
+void wordsCounter();
+void setWord(WordList* wList, char* str);
+void insertWord(WordList* wlist, char* str, int index);
+void shift(WordList* wList, int index);
+void charsCounter();
+void count(int* chars, char* str);
+void printNlines(int N);
+#endif
