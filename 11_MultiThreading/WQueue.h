0d8501a27990313920df09e26ea7c51438687963
alexl-87 <34104341+alexl-87@users.noreply.github.com>
2019-09-11 09:40:51 +0300

    Delete WQueue.h

--- 11_MultiThreading/WQueue.h
+++
@@ -1,9 +0,0 @@
-#ifndef WQueue
-#define WQueue
-
-typedef struct Queue Queue;
-void createQueue(Queue** queue, unsigned int capacity);
-void writeQueue(Queue* queue, void* msg);
-void readQueue(Queue* queue, void** msg);
-
-#endif
