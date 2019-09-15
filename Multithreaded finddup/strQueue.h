f70fce2c15af3856c7801a2e544d2dd6fd55d4ca
alexl-87 <34104341+alexl-87@users.noreply.github.com>
2019-09-15 08:58:03 +0300

    Add files via upload

---
+++ Multithreaded finddup/strQueue.h
@@ -0,0 +1,11 @@
+#ifndef strQueue
+#define strQueue
+
+typedef struct SQueue SQueue;
+typedef struct SNode SNode;
+
+void createSQueue(SQueue** sQueue);
+void SQueuePush(SQueue* queue, char* path);
+void SQueuePull(SQueue* queue, char** path);
+
+#endif
