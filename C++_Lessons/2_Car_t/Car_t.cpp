5c2beb882b03d0afaff617547145fd6956727a9f
alexl-87 <alexlmailbox@gmail.com>
2019-09-25 16:14:21 +0300

    Wed Sep 25 16:14:21 IDT 2019

--- C++_Lessons/2_Car_t/Car_t.cpp
+++ C++_Lessons/2_Car_t/Car_t.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 size_t car_t::id_gen = 1;
+unsigned short car_t:def_eng_capacity = 1500;
 
 car_t::car_t():m_id(id_gen++), m_gear(standart)
 {
--- C++_Lessons/2_Car_t/Car_t.h
+++ C++_Lessons/2_Car_t/Car_t.h
@@ -15,11 +15,15 @@ protected:
 	const size_t m_id;
 	unsigned short m_seatsCapacity;
 	unsigned short m_engineCapacity;
+	unsigned short m_speed;
 	Gear m_gear;
 	string m_name;
 
 private:
 	static size_t id_gen;
+	static unsigned short def_eng_capacity;
+	void setName();
+	void setGear();
 };
 
 #endif
