632b0bbddc12cb9d8633621ff4dfd9830268b76a
alexl-87 <alexlmailbox@gmail.com>
2019-09-25 16:25:54 +0300

    Wed Sep 25 16:25:54 IDT 2019

--- C++_Lessons/2_Car_t/Car_t.h
+++ C++_Lessons/2_Car_t/Car_t.h
@@ -5,25 +5,28 @@
 using namespace std;
 class car_t
 {
-
 public:
 	car_t();
+	car_t(string name, unsigned short engCapacity);
+	void setSpeed(unsigned short seed);
+	unsigned short getSpeed();
 	typedef enum Gear{ automatic, standart }Gear;
 
 protected:
 	~car_t();
 	const size_t m_id;
-	unsigned short m_seatsCapacity;
-	unsigned short m_engineCapacity;
 	unsigned short m_speed;
-	Gear m_gear;
-	string m_name;
+
 
 private:
 	static size_t id_gen;
 	static unsigned short def_eng_capacity;
+	Gear m_gear;
+	string m_name;
+	unsigned short m_engineCapacity;
 	void setName();
 	void setGear();
+	void setEngineCapacity();
 };
 
 #endif
