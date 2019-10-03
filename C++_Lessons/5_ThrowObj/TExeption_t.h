0e58a7feb51cd515c9538d0f96fb62b8c1abe3d4
alexl-87 <alexlmailbox@gmail.com>
2019-10-03 18:14:14 +0300

    Thu Oct  3 18:14:14 IDT 2019

---
+++ C++_Lessons/5_ThrowObj/TExeption_t.h
@@ -0,0 +1,17 @@
+#ifndef TExeption_t
+
+template <class P1, class P2, class P3, class P4>
+class tExeption_t
+{
+	
+public:
+	tExeption_t();
+	~tExeption_t();
+
+private:
+	
+	
+};
+
+#define TExeption_t
+#endif
