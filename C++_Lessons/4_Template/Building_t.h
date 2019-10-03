b5e5a8e36a5251c1f3e6df4ff00b2703e5920ef9
alexl-87 <alexlmailbox@gmail.com>
2019-10-03 15:35:14 +0300

    Thu Oct  3 15:35:14 IDT 2019

--- C++_Lessons/4_Template/Building_t.h
+++ C++_Lessons/4_Template/Building_t.h
@@ -1,52 +1,53 @@
 #ifndef Building_t
 #define Building_t
 
-template <class ID>
+template <class B_ID>
 class building_t
 {
 public:
-	building_t(ID id);
+
+	~building_t();
+	building_t(B_ID id);
 	building_t(building_t& building);
 	void operator=(building_t& building);
-	~building_t();
 
-	void setID(ID id);
-	ID getID() const;
+	void setID(B_ID id);
+	B_ID getID() const;
 
 
 private:
-	ID m_id;
+	B_ID m_id;
 };
 
-template <class ID>
-building_t<ID>::building_t(ID id)
+template <class B_ID>
+building_t<B_ID>::building_t(B_ID id)
 {
 	m_id = id;
 }
 
-template <class ID>
-building_t<ID>::building_t(building_t& building)
+template <class B_ID>
+building_t<B_ID>::building_t(building_t& building)
 {
 	m_id = building.m_id;
 }
 
-template <class ID>
-void building_t<ID>::operator=(building_t& building)
+template <class B_ID>
+void building_t<B_ID>::operator=(building_t& building)
 {
 	m_id = building.m_id;
 }
 
-template <class ID>
-building_t<ID>::~building_t(){}
+template <class B_ID>
+building_t<B_ID>::~building_t(){}
 
-template <class ID>
-ID building_t<ID>::getID() const
+template <class B_ID>
+B_ID building_t<B_ID>::getID() const
 {
 	return m_id;
 }
 
-template <class ID>
-void building_t<ID>::setID(ID id)
+template <class B_ID>
+void building_t<B_ID>::setID(B_ID id)
 {
 	m_id = id;
 }
--- C++_Lessons/4_Template/Street_t.h
+++ C++_Lessons/4_Template/Street_t.h
@@ -1,15 +1,81 @@
 #ifndef Street_t
 #define Street_t
+#include "Building_t.h"
+#include <vector>
+using namespace std;
 
+template <class S_ID, class B_ID>
 class street_t
 {
-	
+
 public:
-	street_t();
+
 	~street_t();
-	
+	street_t(S_ID id);
+	street_t(street_t& street);
+	void operator=(street_t& street);
+
+	S_ID getId()const;
+	void setId(S_ID id);
+	void addBuilding(B_ID);
+	building_t<B_ID> getBuilding()const;
+	building_t<B_ID> getBuilding(unsigned int index)const;
+
+private:
+	S_ID m_id;
+	vector< building_t<B_ID> > m_v;
 };
 
+template <class S_ID, class B_ID>
+street_t <S_ID, B_ID>::~street_t(){}
+
+template <class S_ID, class B_ID>
+street_t <S_ID, B_ID>::street_t(S_ID id)
+{
+
+}
+
+template <class S_ID, class B_ID>
+street_t <S_ID, B_ID>::street_t(street_t& street)
+{
+	m_id = street.m_id;
+}
+
+template <class S_ID, class B_ID>
+void street_t<S_ID, B_ID>::operator=(street_t& street)
+{
+	m_id = street.m_id;
+}
+
+template <class S_ID, class B_ID>
+S_ID street_t<S_ID, B_ID>::getId()const
+{
+	return m_id;
+}
+
+template <class S_ID, class B_ID>
+void street_t <S_ID, B_ID>::setId(S_ID id)
+{
+	m_id = id;
+}
+
+template <class S_ID, class B_ID>
+void street_t <S_ID, B_ID>::addBuilding(B_ID)
+{
+
+}
+
+template <class S_ID, class B_ID>
+building_t<B_ID> street_t <S_ID, B_ID>::getBuilding()const
+{
+
+}
+
+template <class S_ID, class B_ID>
+building_t<B_ID> street_t <S_ID, B_ID>::getBuilding(unsigned int index)const
+{
+
+}
 
 
 #endif
