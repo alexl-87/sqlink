71cc6edd1ff7c2646cb465738f6482f52dca8d7e
alexl-87 <alexlmailbox@gmail.com>
2019-10-03 13:47:39 +0300

    Thu Oct  3 13:47:39 IDT 2019

--- C++_Lessons/4_Template/Building_t.h
+++ C++_Lessons/4_Template/Building_t.h
@@ -1,6 +1,26 @@
 #ifndef Building_t
 #define Building_t
 
+template <class ID>
+class building_t
+{
+public:
+	building_t();
+	~building_t();
+
+	void setID();
+	ID getID();
+
+
+private:
+	ID id;
+};
+
+
+building_t();
+
+
+
 
 
 
--- C++_Lessons/4_Template/Main.cpp
+++ C++_Lessons/4_Template/Main.cpp
@@ -1,6 +1,6 @@
 #include "Building_t.h"
 #include "Street_t.h"
-#include "City_t_t.h"
+#include "City_t.h"
 
 int main(int argc, char const *argv[])
 {
