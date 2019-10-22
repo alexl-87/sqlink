148e677ac624d3eec63945f925eeadd5568b2524
alexl-87 <alexlmailbox@gmail.com>
2019-10-22 14:20:07 +0300

    Tue Oct 22 14:20:07 IDT 2019

--- C++_Lessons/8_IO/virtIO_t.h
+++ C++_Lessons/8_IO/virtIO_t.h
@@ -1,21 +1,34 @@
 #include <string>
+#include <stdio.h>
 using namespace std;
 class virtIO_t
 {
 public:
 	
-	~virtIO_t();
-	virtIO_t();
-
-
 	enum status{ok_e, cant_open_file_e, bad_access_e, writeErr_e, readErr_e};
 	enum access{r, w, rw};
 
+
+	~virtIO_t();
+	virtIO_t();
+	virtIO_t(const string& path, enum access _access);
+	const string& getPath()const;
+	enum access getAccess()const;
+	enum status getStatus()const;
+	void setStatus(enum status _status);
+	enum status{ok_e, cant_open_file_e, bad_access_e, writeErr_e, readErr_e};
+	enum access{r, w, rw};
+	unsigned int getPosition()const;
+	void setPosition(unsigned int position);
+	virtIO_t& operator>>(int num);
+	virtIO_t& operator<<(int num);
+
 protected:
-	enum status stat;
-	enum access acsessMode;
-	string path;
-	FILE* file;
+	enum status m_status;
+	enum access m_access;
+	unsigned int m_fileLeght;
+	string m_path;
+	FILE* m_file;
 
 private:
 	virtIO_t(virtIO_t& v);
