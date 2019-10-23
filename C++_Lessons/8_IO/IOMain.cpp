30d6974e0dcc09190e8d6d90e97cf89eb9f944b3
alexl-87 <alexlmailbox@gmail.com>
2019-10-23 16:55:58 +0300

    Wed Oct 23 16:55:58 IDT 2019

--- C++_Lessons/8_IO/IOMain.cpp
+++ C++_Lessons/8_IO/IOMain.cpp
@@ -9,44 +9,6 @@ void tryAscci();
 void tryBinary();
 int main(int argc, char const *argv[])
 {
-	char opt = ' ';
-	cout << "Ascii or binary, that is the question - a/b?" << endl;
-	cin>>opt;
-	if(opt == 'a') tryAscci();
-	if(opt == 'b') tryBinary();
 
 	return 0;
-}
-
-void tryAscci()
-{
-	int opt = 0;
-	int num = 0;
-	asciiIO_t asc;
-	asc.Open("test.txt", "r");
-	while(true)
-	{
-		cout<<"1 - read\t2 - write"<<endl;
-		cin>>opt;
-
-		switch(opt)
-		{
-			case 1:
-			asc>>num;
-			cout<<num<<endl;
-			break;
-
-			case 2:
-			cout << "Insert value" << endl;
-			cin >> num;
-			asc<<num;
-			break;
-		};
-	}
-
-}
-
-void tryBinary()
-{
-	binIO_t bin;
 }
