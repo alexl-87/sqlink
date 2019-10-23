ff949e5c890d7c7a1b0a70662efaaa2af0cf3771
alexl-87 <alexlmailbox@gmail.com>
2019-10-23 09:08:08 +0300

    Wed Oct 23 09:08:08 IDT 2019

--- C++_Lessons/8_IO/IOMain.cpp
+++ C++_Lessons/8_IO/IOMain.cpp
@@ -6,6 +6,10 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
 	asciiIO_t t;
-
+	asciiIO_t::mode m = asciiIO_t::r;
+	t.fOpen("test.txt", m);
+	int num = 0;
+	t>>num;
+	cout << num << endl;
 	return 0;
 }
