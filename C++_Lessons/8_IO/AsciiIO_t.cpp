120ca3bfee5e2637b3df7b5c80c4861421dbbc38
alexl-87 <alexlmailbox@gmail.com>
2019-10-24 09:04:06 +0300

    Thu Oct 24 09:04:06 IDT 2019

--- C++_Lessons/8_IO/AsciiIO_t.cpp
+++ C++_Lessons/8_IO/AsciiIO_t.cpp
@@ -9,8 +9,8 @@ asciiIO_t::asciiIO_t(const string& path, const string& mode)
 asciiIO_t::~asciiIO_t(){}
 
 
-asciiIO_t& asciiIO_t::operator>>(short& num){return Read(num, "%d");}
-asciiIO_t& asciiIO_t::operator<<(short num){return Write(num, "%d");}
+asciiIO_t& asciiIO_t::operator>>(short& num){return Read(num, "%hd");}
+asciiIO_t& asciiIO_t::operator<<(short num){return Write(num, "%hd");}
 
 asciiIO_t& asciiIO_t::operator>>(int& num){return Read(num, "%d");}
 asciiIO_t& asciiIO_t::operator<<(int num){return Write(num, "%d");}
@@ -30,8 +30,8 @@ asciiIO_t& asciiIO_t::operator<<(double num){return Write(num, "%f");}
 asciiIO_t& asciiIO_t::operator>>(unsigned short& num){return Read(num, "%d");}
 asciiIO_t& asciiIO_t::operator<<(unsigned short num){return Write(num, "%d");}
 
-asciiIO_t& asciiIO_t::operator>>(unsigned int& num){return Read(num, "%d");}
-asciiIO_t& asciiIO_t::operator<<(unsigned int num){return Write(num, "%d");}
+asciiIO_t& asciiIO_t::operator>>(unsigned int& num){return Read(num, "%u");}
+asciiIO_t& asciiIO_t::operator<<(unsigned int num){return Write(num, "%u");}
 
 asciiIO_t& asciiIO_t::operator>>(unsigned long& num){return Read(num, "%lu");}
 asciiIO_t& asciiIO_t::operator<<(unsigned long num){return Write(num, "%lu");}
